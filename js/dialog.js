dbaecdd78716614b38ada1d13a2fe5ba7d40c63e
EndoMotoki <motoki@xmilkyway.com>
2021-03-10 06:12:40 +0900

    change

--- js/dialog.js
+++ js/dialog.js
@@ -1,6 +1,16 @@
-function dialog(){
+function promotion(){
     if(window.confirm('当サービスの結果は100%正確とは限らないため、如何なる結果でも必ずご自身でもチェックをしてください。同意できますか？')) {
         location.href = "promotion.html";
+    }
+    else {
+        window.alert('同意できない場合はサービスを提供できません。ありがとうございました。')
+        location.href = "index.html";
+    }
+}
+
+function graduation(){
+    if(window.confirm('当サービスの結果は100%正確とは限らないため、如何なる結果でも必ずご自身でもチェックをしてください。同意できますか？')) {
+        location.href = "graduation.html";
     }
     else {
         window.alert('同意できない場合はサービスを提供できません。ありがとうございました。')
